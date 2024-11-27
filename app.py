d6ec4aeba803fb8c2ee0a8ed914538ddfad21b3c
Amitkumaryadav82 <49905324+Amitkumaryadav82@users.noreply.github.com>
2024-11-27 04:21:00 +0000

    Inital commit for app.py

---
+++ app.py
@@ -0,0 +1,120 @@
+import json
+import os
+import sys
+import boto3
+import streamlit as st
+import vectorGeneration as vg
+
+
+## LLm Models
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+## Bedrock Clients
+bedrock=boto3.client(service_name="bedrock-runtime")
+bedrock_embeddings=BedrockEmbeddings(model_id="amazon.titan-embed-text-v1",client=bedrock)
+
+
+def get_claude_llm():
+    ##create the Anthropic Model
+    llm=Bedrock(model_id="ai21.j2-mid-v1",client=bedrock,
+                model_kwargs={'maxTokens':512})
+    
+    return llm
+
+def get_llama2_llm():
+    ##create the Anthropic Model
+    llm=Bedrock(model_id="meta.llama2-70b-chat-v1",client=bedrock,
+                model_kwargs={'max_gen_len':512})
+    
+    return llm
+
+prompt_template = """
+
+Human: Use the following pieces of context to provide a 
+concise answer to the question at the end. If you don't know the answer, 
+just say that you don't know, don't try to make up an answer.
+<context>
+{context}
+</context
+
+Question: {question}
+
+Assistant:"""
+
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+def get_response_llm(llm,vectorstore_faiss,query):
+    qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore_faiss.as_retriever(
+        search_type="similarity", search_kwargs={"k": 3}
+    ),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": PROMPT}
+)
+    answer=qa({"query":query})
+    return answer['result']
+
+##Only for local testing. We will using lambda to invoke the LLM via Bedrock
+def main():
+    st.set_page_config("Lease Manager")
+    
+    st.header("Lease Manager")
+
+    user_question = st.text_input("Ask a Question Related With Leases")
+
+    with st.sidebar:
+        st.title("Add More Leases and update system:")
+        
+        ## this button will not allow you to add the files rather it will update the vector store
+        ## @TODO: Perhaps we should provide a button to add files into the S3 bucket here.
+        if st.button("Update DB"):
+            with st.spinner("Processing..."):
+                docs = data_ingestion()
+                get_vector_store(docs)
+                st.success("Done")
+
+
+    if st.button("Llama2 Output"):
+        with st.spinner("Processing..."):
+           faiss_index= vg.read_faiss_s3(s3_key,s3_bucket)
+           # faiss_index = FAISS.load_local("faiss_index", bedrock_embeddings)
+            llm=get_llama2_llm()
+            
+            #faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm,faiss_index,user_question))
+            st.success("Done")
+
+if __name__ == "__main__":
+    main()
+
+
+## Since we will using AWS lambda to invoke LLM, we need to write a handler
+def lambda_handler(event, context):
+    # TODO implement
+    event=json.loads(event['body'])
+    #@TODO: Update the event after the API Gateway configuration
+    blogtopic=event['blog_topic']
+
+    generate_blog=blog_generate_using_bedrock(blogtopic=blogtopic)
+
+    if generate_blog:
+        current_time=datetime.now().strftime('%H%M%S')
+        s3_key=f"blog-output/{current_time}.txt"
+        s3_bucket='aws_bedrock_course1'
+        save_blog_details_s3(s3_key,s3_bucket,generate_blog)
+
+
+    else:
+        print("No blog was generated")
+
+    return{
+        'statusCode':200,
+        'body':json.dumps('Blog Generation is completed')
+    }
+
+    
