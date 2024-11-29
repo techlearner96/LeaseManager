d0fae837dfab37990d7277d29fbfbc0ede64d526
Amitkumaryadav82 <49905324+Amitkumaryadav82@users.noreply.github.com>
2024-11-29 03:57:39 +0000

    Added Lambda handler and changed the code a lot

--- vectorGenerator.py
+++ vectorGenerator.py
@@ -1,25 +1,39 @@
 ## This will be a separate lambda which will be used to generate the vectors
 
 import numpy as np
+import json
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.vectorstores import FAISS
 
-## This function will read the data and  split the documents into chunk
-##@TODO: PyPDF may not give the best result. WE need to try calling llamaParser API and see if that works better
-def data_splitter():
-    loader=PyPDFDirectoryLoader("data")
-    documents=loader.load()
+s3_key="faiss_index"
+s3_bucket="capleasemanager/lease"
 
+#Read the documents
+def read_documents_from_s3(bucket_name):
+    # List objects in the bucket
+    response = s3.list_objects_v2(Bucket=bucket_name)
+    documents = []
+    
+    for obj in response.get('Contents', []):
+        # Read each document
+        file_obj = s3.get_object(Bucket=bucket_name, Key=obj['Key'])
+        document = file_obj['Body'].read().decode('utf-8')
+        documents.append(document)
+    
+    return documents
+
+## This function will read the data and  split the documents into chunk
+def data_splitter(documents):
     # - in our testing Character split works better with this PDF data set
     text_splitter=RecursiveCharacterTextSplitter(chunk_size=10000,
                                                  chunk_overlap=1000)
     
-    docs=text_splitter.split_documents(documents)
-    return docs
+    split_docs=text_splitter.split_documents(documents)
+    return split_docs
 
 ## Generate the faiss vecotrs from the documents
-def generate_faiss(docs):
+def generate_faiss(split_docs):
     vectorstore_faiss=FAISS.from_documents(
         docs,
         bedrock_embeddings
@@ -27,7 +41,7 @@ def generate_faiss(docs):
     return vectorstore_faiss
 
 ## save the generated faiss vectors in S3
-def save_faiss_s3(s3_key,s3_bucket,faiss_embed):
+def save_faiss_s3(faiss_embed):
     s3=boto3.client('s3')
 
     try:
@@ -36,3 +50,19 @@ def save_faiss_s3(s3_key,s3_bucket,faiss_embed):
 
     except Exception as e:
         print("Error when saving the code to s3")
+
+
+def lambda_handler(event, context):
+    #read the documents
+    documents = read_documents_from_s3(s3_bucket)
+    #split document
+    split_doc = data_splitter(documents)
+    #generate faiss
+    faiss_index = generate_faiss(split_docs)
+    #Store faiss in S3
+    save_faiss_s3(faiss_index)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
