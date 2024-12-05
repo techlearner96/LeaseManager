5bb475142c53c7a3fc4f12809df9448d45c6b5a8
Amitkumaryadav82 <49905324+Amitkumaryadav82@users.noreply.github.com>
2024-12-05 04:51:15 +0000

    Code all integrated. Final testing needed

--- vectorGenerator.py
+++ vectorGenerator.py
@@ -5,46 +5,66 @@ import json
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.vectorstores import FAISS
+from langchain.schema import Document
 from io import BytesIO
 import boto3
 import os
 import pytesseract
 from PIL import Image
+from langchain_community.embeddings import BedrockEmbeddings
 
-s3_key="faiss_index"
-s3_bucket="capleasemanager/lease"
+
+bucket_name = 'capleasemanager'
+prefix = 'lease/'
+s3_faiss='faiss/'
+bedrock=boto3.client(service_name="bedrock-runtime")
+bedrock_embeddings=BedrockEmbeddings(model_id="amazon.titan-embed-text-v1",client=bedrock)
+
 
 #Get the documents
-def get_documents_from_s3(s3_bucket):
+def get_documents_from_s3(s3_bucket, prefix):
     # @TODO  test with S3
-    # List objects in the bucket
-    # s3=boto3.client("s3")
-    # response = s3.list_objects_v2(Bucket=bucket_name)
-    # documents = []
-    
-    # for obj in response.get('Contents', []):
-    #     # Read each document
-    #     file_obj = s3.get_object(Bucket=bucket_name, Key=obj['Key'])
-    #     document = file_obj['Body'].read().decode('utf-8')
-    #     documents.append(document)
-    #     return documents
-    # This code is for local testing only
-    folder_path = 'testdata/'
-    text_list = []
+    s3=boto3.client("s3")
+    response = s3.list_objects_v2(Bucket=bucket_name)
+    files = response.get('Contents', [])
 
-    # Iterate over each file in the folder
-    for filename in os.listdir("testdata/"):
-        if filename.endswith('.png'):
-            # Open the image file
-            img_path = os.path.join("testdata/", filename)
-            img = Image.open(img_path)
-            
-            # Use pytesseract to extract text from the image
-            text = pytesseract.image_to_string(img)
-            
+    text_list=[] 
+    for file in files:
+        file_key = file['Key']
+        if file_key.endswith('.png'):
+            # Download the file from S3
+            obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+            img_data = obj['Body'].read()
+
+            # Open the image
+            image = Image.open(BytesIO(img_data))
+
+            # Perform OCR on the image
+            text = pytesseract.image_to_string(image)
+
             # Append the extracted text to the list
             text_list.append(text)
-            return text_list
+
+    return text_list
+
+    
+    # # This code is for local testing only
+    # folder_path = 'testdata/'
+    # text_list = []
+
+    # # Iterate over each file in the folder
+    # for filename in os.listdir("testdata/"):
+    #     if filename.endswith('.png'):
+    #         # Open the image file
+    #         img_path = os.path.join("testdata/", filename)
+    #         img = Image.open(img_path)
+            
+    #         # Use pytesseract to extract text from the image
+    #         text = pytesseract.image_to_string(img)
+            
+    #         # Append the extracted text to the list
+    #         text_list.append(text)
+    #         return text_list
 
     
 
@@ -80,9 +100,10 @@ def data_splitter(documents):
 
    # Generate the FAISS vectors from the documents
 def generate_faiss(split_docs):
+    documents = [Document(page_content=text) for text in split_docs]
     vectorstore_faiss = FAISS.from_documents(
         split_docs,
-        bedrock_embeddings  # Ensure bedrock_embeddings is defined
+        bedrock_embeddings  
     )
     return vectorstore_faiss
 
@@ -94,24 +115,33 @@ def save_faiss_s3(faiss_index):
     faiss_bytes.seek(0)
 
     try:
-        s3.put_object(Bucket=s3_bucket, Key=s3_key, Body=faiss_bytes.getvalue())
+        s3.put_object(Bucket=bucket_name, Key=s3_faiss, Body=faiss_bytes.getvalue())
         print("FAISS index saved to S3")
     except Exception as e:
         print(f"Error when saving the FAISS index to S3: {e}")
 
 
 def generate_vectors():
-    raw_png = get_documents_from_s3(s3_bucket)
+    list_text = get_documents_from_s3(bucket_name,prefix)
     
-    list_text=read_text_from_files(raw_png)
+    # list_text=read_text_from_files(raw_png)
     splitted_text= data_splitter(list_text)
+    vectorstore_faiss= generate_faiss(splitted_text)
+    save_faiss_s3(vectorstore_faiss)
     
-    for i, chunk in enumerate(splitted_text):
-        print(f"Chunk {i+1}: {chunk}")
     
 
 if __name__ == "__main__":
     generate_vectors()
+    # # Example usage
+    # bucket_name = 'capleasemanager'
+    # prefix = 'lease/'
+    # text_list = get_documents_from_s3(bucket_name, prefix)
+
+    # # Print the list of extracted texts
+    # for text in text_list:
+    #     print(text)
+    
 
 #Commenting this function since I will not be using Lambda  anymore
 ###def lambda_handler(event, context):
