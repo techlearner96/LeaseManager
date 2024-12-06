06d433e6f19894f23dded7a64242c86e85f4cb6e
techlearner96 <107695196+techlearner96@users.noreply.github.com>
2024-12-06 08:38:56 +0000

    Added component to upload more than one files and upload it to s3

--- leaseManagerUI.py
+++ leaseManagerUI.py
@@ -1,8 +1,11 @@
 ## This file will contain the code realted with App's UI. It will need to deployed as application
 ### on EC2. Another option is to dockerize it and deploy it on ECS
-
-
+import boto3
 import streamlit as st
+from botocore.exceptions import ClientError
+
+bucket_name = 'capleasemanager'
+
 
 ##Only for local testing. We will using lambda to invoke the LLM via Bedrock
 def main():
@@ -14,20 +17,29 @@ def main():
 
     with st.sidebar:
         st.title("Add More Leases and update system:")
-        
-        ## this button will not allow you to add the files rather it will update the vector store
-        ## @TODO: Perhaps we should provide a button to add files into the S3 bucket here.
-        if st.button("Update DB"):
+        uploaded_file = st.file_uploader("Choose lease pages to upload", type=["png", "jpeg"], accept_multiple_files=True)
+        if st.button("Upload Lease Data"):
             with st.spinner("Processing..."):
-                #@TODO Add the code to invoke the vectorGenerator API via API Gateway
+                '''Storing the files to s3'''
+                s3_client = boto3.client('s3')
+                for file in uploaded_file:
+                    try:
+                        response = s3_client.upload_fileobj(file, bucket_name, file.name)
+                    except ClientError as e:
+                        raise Exception(f"error in uploading the file")
+                '''
+                  2. Call endpoint to generate vector from vectorGenerator.py
+                  3. Here we need to update database but how?
+                '''
                 st.success("Done")
 
 
     if st.button("Output"):
         with st.spinner("Processing..."):
            #@TODO Add code to (1) invoke the getLeaseInfo API via APIGateway
-
+            response = 'response of query' # call the api to get response for the query through getLeaseInfo
             st.success("Done")
+        st.write(response)
 
 if __name__ == "__main__":
     main()
